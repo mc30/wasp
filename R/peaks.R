1492b9a3721a1f640654c0afc6aec60eea286aaf
mc30 <mikhail.churakov@gmail.com>
2017-10-16 17:23:44 +0200

    Added documentation for peaks functions.

--- R/peaks.R
+++ R/peaks.R
@@ -3,6 +3,17 @@
 ###############################################
 
 
+#' @title Get matrix of peaks
+#' @description Creates a matrix with peak timing for each season.
+#'
+#' @param cases A matrix of case series.
+#' @param start The start timepoint of each season.
+#' @param period The period of seasons (e.g. 12 for monthly data, and 52 for weekly).
+#' 
+#' @return Returns a matrix with peak timing for each season and location.
+#'
+#' @author Mikhail Churakov (\email{mikhail.churakov@@pasteur.fr}).
+#' 
 #' @export
 getPeaksMatrix <- function(cases, start, period = 12) {
   for (loc in 1:nrow(cases)) {
@@ -27,9 +38,18 @@ getPeaksMatrix <- function(cases, start, period = 12) {
 }
 
 
-
+#' @title Plot annual curves
+#' @description Plots seasonal curves for each location with peaks and mean peak timing.
+#'
+#' @param cases A matrix of case series.
+#' @param start The start timepoint of each season.
+#' @param period The period of seasons (e.g. 12 for monthly data, and 52 for weekly).
+#' @param perc CI range for the mean peak timing.
+#' 
+#' @author Mikhail Churakov (\email{mikhail.churakov@@pasteur.fr}).
+#' 
 #' @export
-plotAnnualCurves <- function(cases, start, period = 12) {
+plotAnnualCurves <- function(cases, start, period = 12, perc = 0.95) {
   for (loc in 1:nrow(cases)) {
     timeStep <- period
     time <- c(start:(start + timeStep))
@@ -69,12 +89,26 @@ plotAnnualCurves <- function(cases, start, period = 12) {
 }
 
 
+#' @title Plot mean peak timing
+#' @description Plot mean peak timing for all locations.
+#'
+#' @param pMat A matrix with peaks for each location and season.
+#' @param start The start timepoint of each season.
+#' @param period The period of seasons (e.g. 12 for monthly data, and 52 for weekly).
+#' @param perc CI range for the mean peak timing.
+#' @param Logical; if \code{TRUE} bootstrap will be used.
+#' 
+#' @return Returns a named vector of mean peaks for each location.
+#'
+#' @author Mikhail Churakov (\email{mikhail.churakov@@pasteur.fr}).
+#' 
 #' @export
-plotMeanPeakTiming <- function(pMat, start, period = 12, bBootstrap = FALSE) {
+plotMeanPeakTiming <- function(pMat, start, period = 12, perc = 0.95, bBootstrap = FALSE) {
   
   # Ordinary mean
   meanPeaks <- sapply(1:nrow(pMat), function(i) mean(pMat[i, ]))
   
+  # ci <- qnorm(perc + (1 - perc) / 2) / sqrt(length(peaks)) # Coefficient to get CI from SD (normal distribution)
   ci <- qt(perc + (1 - perc) / 2, df = ncol(pMat) - 1) / sqrt(ncol(pMat)) # Coefficient to get CI from SD (t-distribution)
   
   sds <- sapply(1:nrow(pMat), function(i) sd(pMat[i, ]))
@@ -82,22 +116,15 @@ plotMeanPeakTiming <- function(pMat, start, period = 12, bBootstrap = FALSE) {
   upper <- meanPeaks + ci * sds
   lower <- meanPeaks - ci * sds
   
-  
   # Bootstrapping
   if (bBootstrap) {
     mboot <- sapply(1:nrow(pMat), function(i) boot(pMat[i, ], function(x, k) mean(x[k]), R = 10000)$t[, 1])
     meanPeaks <- sapply(1:nrow(pMat), function(i) mean(mboot[, i]))
     
-    # x <- seq(-0.5, 0.5, length.out = length(meanPeaks))
-    # sapply(1:nrow(pMat), function(i)
-    #   lines(c(x[order(order(meanPeaks2))[i]], x[order(order(meanPeaks2))[i]]), quantile(mboot[, i], probs = c(0.025, 0.975)), col = "blue")
-    # )
-    
-    lower <- sapply(1:nrow(pMat), function(i) quantile(mboot[, i], probs = c(0.025)))
-    upper <- sapply(1:nrow(pMat), function(i) quantile(mboot[, i], probs = c(0.975)))
+    lower <- sapply(1:nrow(pMat), function(i) quantile(mboot[, i], probs = (1 - perc) / 2))
+    upper <- sapply(1:nrow(pMat), function(i) quantile(mboot[, i], probs = perc + (1 - perc) / 2))
   }
   
-  # meanPeaks <- setNames(meanPeaks, rownames(pMat))
   names(meanPeaks) <- rownames(pMat)
   
   old.par <- par(mar = c(5.1 + 2.5, 4.1, 4.1, 2.1))
@@ -105,8 +132,6 @@ plotMeanPeakTiming <- function(pMat, start, period = 12, bBootstrap = FALSE) {
   upper <- upper[order(meanPeaks)]
   lower <- lower[order(meanPeaks)]
   meanPeaks <- sort(meanPeaks)
-  
-  mNames <- names(meanPeaks)
   
   plot(c(0, 0), range(upper, lower), cex = 0, 
        xaxt = "n", yaxt = "n", 
@@ -123,7 +148,7 @@ plotMeanPeakTiming <- function(pMat, start, period = 12, bBootstrap = FALSE) {
   points(x, meanPeaks, col = "red", pch = 19)
   for (i in order(row.names(cases)))
     lines(c(x[i], x[i]), c(lower[i], upper[i]), col = "red")
-  axis(side = 1, at = x, labels = mNames, las = 2, cex.axis = 0.6)
+  axis(side = 1, at = x, labels = names(meanPeaks), las = 2, cex.axis = 0.6)
   par(old.par)
   
   return(meanPeaks)
